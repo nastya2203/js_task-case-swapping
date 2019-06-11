24d6db0e9448e52d2f6fdda8e8eadf08fa1e82dd
vpolets <vpolets@gmail.com>
2019-06-11 16:26:21 +0300

    add new test

--- src/case-swapping.test.js
+++ src/case-swapping.test.js
@@ -21,3 +21,8 @@ test('Should swap case only for letter and ignore special characters', () => {
   expect(swap('h_e_L-L_0 wo|||rLD'))
     .toBe('H_E_l-l_0 WO|||Rld');
 });
+
+test('Should not change number string', () => {
+  expect(swap('123235'))
+    .toBe('123235');
+});
